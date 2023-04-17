eb3f5685345a81a5ac1d99faf65121325f625b48
alpha-gone <990131@naver.com>
2023-04-17 13:54:13 +0900

    스프링 프로젝트 멀티 모듈 적용

---
+++ api-server/heartbeat-rdb/src/test/java/com/github/heartbeat/rdb/HeartbeatRdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.heartbeat.rdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatRdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
